a9ee24a9dd5b3d94034dddc60c18bf94909f7fbd
Luiz Carlos de O. Nolasco <13737927+lconolasco@users.noreply.github.com>
2021-01-02 00:58:01 +0100

    Create Project
    Create Solution of Tris

---
+++ trisPage.xaml.cs
@@ -0,0 +1,1 @@
+﻿using Xamarin.Forms;namespace tris{    public partial class trisPage : ContentPage    {        public trisPage()        {            InitializeComponent();        }    }}
